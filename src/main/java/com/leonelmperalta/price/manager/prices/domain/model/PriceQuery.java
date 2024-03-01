1941961694f2780700b813d1b2ec7faf9aa0d02c
Leonel Peralta <leonelm.peralta@outlook.es>
2024-03-01 01:22:30 -0300

    PriceQuery added builder

--- src/main/java/com/leonelmperalta/price/manager/prices/domain/model/PriceQuery.java
+++ src/main/java/com/leonelmperalta/price/manager/prices/domain/model/PriceQuery.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 
 public class PriceQuery {
 
+    private PriceQuery() {}
+
     private LocalDateTime applicationEndDate;
     private LocalDateTime applicationStartDate;
     private Long brandId;
@@ -16,5 +18,59 @@ public class PriceQuery {
 
     public Integer getPriority() {
         return priority;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private final PriceQuery instance = new PriceQuery();
+
+        private Builder() {}
+
+        public Builder applicationEndDate(LocalDateTime applicationEndDate) {
+            instance.applicationEndDate = applicationEndDate;
+            return this;
+        }
+
+        public Builder applicationStartDate(LocalDateTime applicationStartDate) {
+            instance.applicationStartDate = applicationStartDate;
+            return this;
+        }
+
+        public Builder brandId(Long brandId) {
+            instance.brandId = brandId;
+            return this;
+        }
+
+        public Builder currency(String currency) {
+            instance.currency = currency;
+            return this;
+        }
+
+        public Builder feeId(Long feeId) {
+            instance.feeId = feeId;
+            return this;
+        }
+
+        public Builder finalPrice(BigDecimal finalPrice) {
+            instance.finalPrice = finalPrice;
+            return this;
+        }
+
+        public Builder priority(Integer priority) {
+            instance.priority = priority;
+            return this;
+        }
+
+        public Builder productId(Long productId) {
+            instance.productId = productId;
+            return this;
+        }
+
+        public PriceQuery build() {
+            return instance;
+        }
     }
 }
