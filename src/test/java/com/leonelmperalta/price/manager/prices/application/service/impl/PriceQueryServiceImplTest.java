31d34f695c4e3c8ab119330ab262b1bef400f7ac
Leonel Peralta <leonelm.peralta@outlook.es>
2024-03-01 14:53:43 -0300

    Added test case when there isn't any price for the request

--- src/test/java/com/leonelmperalta/price/manager/prices/application/service/impl/PriceQueryServiceImplTest.java
+++ src/test/java/com/leonelmperalta/price/manager/prices/application/service/impl/PriceQueryServiceImplTest.java
@@ -16,6 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -79,6 +80,26 @@ class PriceQueryServiceImplTest {
         assertNotNull(result);
         assertEquals(2, result.getFeeId());
         assertEquals(BigDecimal.valueOf(25.45), result.getFinalPrice());
+    }
+
+    @Test
+    public void givenInvalidArguments_whenPriceQuery_thenReturnNotDataFound()
+            throws InternalServerErrorException {
+        Long brandId = 1L;
+        Long productId = 35455L;
+        String applicationDate = "2020-06-14T16:00:00Z";
+        LocalDateTime applicationDateAsDate = LocalDateTime.of(
+                2020, 6, 14, 16, 0, 0
+        );
+        List<PriceQuery> priceQueries = new ArrayList<>();
+
+        Mockito.when(this.priceQueryServiceMapper.toLocalDateTime(eq(applicationDate))).thenReturn(applicationDateAsDate);
+
+        Mockito.when(this.priceQueryRepository.findByProductIdAndBrandIdAndApplicationDatesBetween(
+                eq(brandId), eq(productId), eq(applicationDateAsDate)
+        )).thenReturn(priceQueries);
+
+        assertThrows(NotDataFoundException.class, () -> this.priceQueryService.priceQuery(brandId, productId, applicationDate));
     }
 
 }
