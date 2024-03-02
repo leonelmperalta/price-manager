583f03927b38eaf6a86881ca90d3460eed644b73
Leonel Peralta <leonelm.peralta@outlook.es>
2024-03-02 18:44:49 -0300

    Fix not data found exception code

--- src/main/java/com/leonelmperalta/price/manager/prices/application/constants/ExceptionConstants.java
+++ src/main/java/com/leonelmperalta/price/manager/prices/application/constants/ExceptionConstants.java
@@ -4,6 +4,6 @@ public class ExceptionConstants {
     public static final String INTERNAL_SERVER_ERROR_CODE = "ERROR_500";
     public static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error.";
 
-    public static final String NOT_DATA_FOUND_CODE = "ERROR_400";
+    public static final String NOT_DATA_FOUND_CODE = "ERROR_404";
     public static final String NOT_DATA_FOUND_MESSAGE = "Not data found for given parameters";
 }
